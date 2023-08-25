ce6dd46404ebc73c8f50942ac3ea405981d6cc7b
Jack Potts <2233184@champlaincollege.qc.ca>
2023-08-25 09:31:34 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Code1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Do you want hawaiian pizza?");
+                var userResponse = Console.ReadLine();
+
+            if (userResponse == "Yes")
+            {
+                Pizza.makeHawaiian();
+
+            }
+            else { Console.WriteLine("The pizza you have ordered is a " + Pizza.PizzaType + " Pizza"); }
+
+
+        }
+
+
+
+
+
+
+
+
+        class Pizza
+        {
+            public static string PizzaType = "Cheese";
+
+            public static void makeHawaiian()
+            {
+                PizzaType = "Hawaiian";
+                Console.WriteLine("The pizza you have ordered is a " + PizzaType + " Pizza");
+            }
+
+                
+                }
+            }
+        }
+    
+
+
